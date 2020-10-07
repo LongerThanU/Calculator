f4ac741c283ba9d076d349232e96346e362b741f
Artemiy <senartemiy@gmail.com>
2020-10-07 15:53:34 +0300

    Initial commit

---
+++ Calculator/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Summ_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Subtract_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Mult_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Del_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Parc_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
